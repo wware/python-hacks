0d9e72617b376b2fd23a51a5efa3414fb0fddc66
Will Ware <wware@veracode.com>
2021-03-29 19:30:59 -0400

    Split out pre- and post-processing as separate functions.
    This allows you to include the function's internal local variables
    in the post conditions.

--- contracts/tryit.py
+++ contracts/tryit.py
@@ -1,9 +1,59 @@
-import pprint
 import inspect
 import os
 import re
 import yaml
 from functools import wraps
+
+
+"""
+# Contractual functions in Python
+
+People call this "design by contract". The easily google-able info
+about it is pretty shallow, unfortunately. But I think there is
+something worthwhile there. See `tryit.py` in this directory for
+the general idea.
+
+If you want to disable the contract stuff and get back any performance
+hit it might have caused, run the code with `python -O` instead of
+`python`, which will turn off the `__debug__` flag.
+
+## Why do this?
+
+Programmers' assumptions are almost never recorded accurately or
+communicated effectively. With contracts, we get:
+
+* Each function has assumptions specified as pre-conditions and
+  post-conditions and invariants.
+* All these assumptions are tested on every function call all the
+  time, unless explicitly switched off for performance.
+* Any failed assumption provides information to minimize the
+  search space for the bug.
+
+If a post-condition fails after the pre-conditions passed, then the
+function is at fault. If a pre-condition fails, then the code that
+calls the function is at fault. If an invariant changes, at least you
+know where to start looking.
+
+You can mix contractual and non-contractual functions, so you don't
+need to upgrade all your code before you can benefit.
+
+Python has a magic variable called "__debug__" which controls normal
+assert statements and also these contract things, and you can switch
+them all off for performance by using the "-O" option when you invoke
+the Python interpreter. I personally think you should keep them on all
+the time, and if you have performance issues, write better code.
+
+## Two modes
+
+The approach here presents two modes of contracts. One uses markup in
+the docstring, combined with a decorator. The other uses the same
+markup but the decorator is replaced by a couple of function calls in
+your function. The latter is a teeny bit more work, but it allows your
+post-conditions to pull in the function's internal local variables,
+which are not reachable by the decorator. So if you include an internal
+variable in a post-condition and then use the decorator, you'll get an
+error.
+"""
 
 
 def env_var(x):
@@ -24,6 +74,67 @@ class PostConditionFailed(ContractViolation):
 
 class InvariantChanged(ContractViolation):
     pass
+
+
+def extract_contracts(docstr):
+    lines = [L.rstrip() for L in docstr.split('\n')]
+    headers = set(['pre', 'post', 'invariant'])
+    contracts = {}
+    while headers:
+        h = headers.pop()
+        p = [i for (i, L) in enumerate(lines) if re.search(h + ":$", L)]
+        if p:
+            yaml_lines = lines[p[0]:]
+            while len(yaml_lines) > 1:
+                try:
+                    contracts.update(yaml.load(
+                        "\n".join(yaml_lines),
+                        Loader=yaml.SafeLoader
+                    ))
+                    headers = headers.difference(contracts.keys())
+                    break
+                except:
+                    pass
+                yaml_lines = yaml_lines[:-1]
+    return contracts
+
+
+__inv_stack = []
+
+
+def check_pre_conditions(locals=None, docstr=None):
+    fr = inspect.currentframe(1)
+    if locals is None:
+        locals = fr.f_locals
+    if docstr is None:
+        docstr = fr.f_globals[fr.f_code.co_name].__doc__
+    contracts = extract_contracts(docstr)
+    for p in contracts.get('pre', []):
+        if not eval(p, globals(), locals):
+            raise PreConditionFailed(p)
+    inv = []
+    for p in contracts.get('invariant', []):
+        inv.append(eval(p, globals(), {}))
+    __inv_stack.append(tuple(inv))
+
+
+def check_post_conditions(ret_val, locals=None, docstr=None):
+    fr = inspect.currentframe(1)
+    if locals is None:
+        locals = fr.f_locals
+    if docstr is None:
+        docstr = fr.f_globals[fr.f_code.co_name].__doc__
+    contracts = extract_contracts(docstr)
+    locals = locals.copy()
+    locals['RETURN'] = ret_val
+    for p in contracts.get('post', []):
+        if not eval(p, globals(), locals):
+            raise PostConditionFailed(p)
+    inv = __inv_stack.pop()
+    for old, p in zip(inv, contracts.get('invariant', [])):
+        x = eval(p, globals(), {})
+        if old != x:
+            raise InvariantChanged(p)
 
 
 def enforce_contract(func):
@@ -57,65 +168,53 @@ def enforce_contract(func):
     """
     if not __debug__:
         return func
-    lines = [L.rstrip() for L in func.__doc__.split('\n')]
-    headers = set(['pre', 'post', 'invariant'])
-    contracts = {}
-    while headers:
-        h = headers.pop()
-        p = [i for (i, L) in enumerate(lines) if re.search(h + ":$", L)]
-        if p:
-            yaml_lines = lines[p[0]:]
-            while len(yaml_lines) > 1:
-                try:
-                    contracts.update(yaml.load(
-                        "\n".join(yaml_lines),
-                        Loader=yaml.SafeLoader
-                    ))
-                    headers = headers.difference(contracts.keys())
-                    break
-                except:
-                    pass
-                yaml_lines = yaml_lines[:-1]
-    co = func.func_code
-    arg_names = co.co_varnames
-    _globals = inspect.currentframe(1).f_globals
 
+    def make_locals(args, kw):
+        locals = dict(zip(func.func_code.co_varnames, args))
+        locals.update(kw)
+        return locals
+    
     @wraps(func)
     def inner(*args, **kw):
-        _locals = dict(zip(arg_names, args))
-        _locals.update(kw)
-
-        def evaluate(c, ouch, _locals=_locals, _globals=_globals):
-            try:
-                return eval(c, _globals, _locals)
-            except Exception as e:
-                raise ouch(pprint.pformat((e, p, _locals, _globals)))
-
-        inv = {}
-        for i, p in enumerate(contracts.get('invariant', [])):
-            inv[i] = evaluate(p, InvariantChanged)
-        for p in contracts.get('pre', []):
-            if not evaluate(p, PreConditionFailed):
-                raise PreConditionFailed("\n" + pprint.pformat((p, _locals, _globals)))
+        check_pre_conditions(make_locals(args, kw), func.__doc__)
         return_val = func(*args, **kw)
-        _locals['RETURN'] = return_val
-        for i, p in enumerate(contracts.get('invariant', [])):
-            newer = evaluate(p, InvariantChanged)
-            if inv[i] != newer:
-                raise InvariantChanged("\n" + pprint.pformat((p, inv[i], newer)))
-        for p in contracts.get('post', []):
-            if not evaluate(p, PostConditionFailed):
-                raise PostConditionFailed("\n" + pprint.pformat((p, return_val, _locals, _globals)))
+        check_post_conditions(return_val, make_locals(args, kw), func.__doc__)
         return return_val
     return inner
+
 
 ################################
 
 
-state_capitals = ['Boston', 'Hartford']
+IMPLACABLE = 'this value does not change and is therefore invariant'
 
 
 @enforce_contract
+def simplest(x, y):
+    """
+    This is a nice simple case. Notice that the post conditions do not reference
+    any variables except the function arguments and the return value.
+
+    pre:
+        - isinstance(x, int)
+        - -5 <= x <= 5
+        - isinstance(y, int)
+        - -5 <= y <= 5
+    post:
+        - isinstance(RETURN, int)
+        - -10 <= RETURN <= 10
+    invariant:
+        - IMPLACABLE
+    """
+    return x + y
+
+
+
+DECORATOR = False
+BAD1 = BAD2 = BAD3 = False
+
+state_capitals = ['Boston', 'Hartford']
+
 def example_function(a, b, c, x, y, z, plugh=None):
     """
     Random information about the function...
@@ -147,17 +246,147 @@ def example_function(a, b, c, x, y, z, plugh=None):
     """
     # use x, y, and z, for nothing, to avoid a pycharm warning
     x.update({'silly': y + list(z), 'extra': plugh})
+    if not DECORATOR:
+        check_pre_conditions()
     return_val = int(11 + 0 * (a + b + c).real)
-    if env_var('BAD1'):
+    if BAD1:
         return_val = 2   # violate post condition
-    if env_var('BAD2'):
+    if BAD2:
         state_capitals.append('Bangor')    # violate invariant
+    if not DECORATOR:
+        check_post_conditions(return_val)
     return return_val
 
 
-if env_var('BAD3'):
-    a = 600      # violate pre condition
-else:
-    a = 3
-print example_function(a, 11.0, 3.0 + 4.0j,
-                       {}, ['1', 2, 3, 4], ())
+example_decorated = enforce_contract(example_function)
+
+
+def example_2(x):
+    """
+    If we use the explicit check... functions, we can access the function's
+    internal local variables, which are not reachable by the decorator. Here
+    we are accessing 'y'.
+
+    post:
+        - x + y < 12
+    """
+    check_pre_conditions()
+    y = x + 3
+    z = x + y
+    check_post_conditions(z)
+    return z
+
+
+def example_3(x, y=1, z=2, **kw):
+    """
+    pre:
+        - y < 5
+        - z < 5
+        - len(kw.keys()) < 2
+    post:
+        - x + y + z < 15
+        - RETURN < 15
+    """
+    check_pre_conditions()
+    w = x * y * z
+    check_post_conditions(w)
+    return w
+
+
+###########################################################################
+
+
+import unittest
+
+
+class SimpleTestCase(unittest.TestCase):
+    def run_example(self):
+        a = 600 if BAD3 else 3
+        return example_function(a, 11.0, 3.0 + 4.0j,
+                                {}, ['1', 2, 3, 4], ())
+
+    def run_decorated(self):
+        a = 600 if BAD3 else 3
+        return example_decorated(a, 11.0, 3.0 + 4.0j,
+                                 {}, ['1', 2, 3, 4], ())
+
+    def test_simple_clean(self):
+        self.assertEqual(simplest(3, 4), 7)
+
+    def test_simple_bad1(self):
+        self.assertRaises(PreConditionFailed,
+                          lambda: simplest(3.14159, 'abc'))
+
+    def test_simple_bad2(self):
+        self.assertRaises(PreConditionFailed,
+                          lambda: simplest(2, 11))
+
+    def test_1_clean(self):
+        global BAD1, BAD2, BAD3, DECORATOR
+        BAD1 = BAD2 = BAD3 = DECORATOR = False
+        self.assertEqual(self.run_example(), 11)
+
+    def test_1_bad1(self):
+        global BAD1, BAD2, BAD3, DECORATOR
+        BAD1 = True
+        BAD2 = BAD3 = DECORATOR = False
+        self.assertRaises(PostConditionFailed, self.run_example)
+
+    def test_1_bad2(self):
+        global BAD1, BAD2, BAD3, DECORATOR
+        BAD2 = True
+        BAD1 = BAD3 = DECORATOR = False
+        self.assertRaises(InvariantChanged, self.run_example)
+
+    def test_1_bad3(self):
+        global BAD1, BAD2, BAD3, DECORATOR
+        BAD3 = True
+        BAD1 = BAD2 = DECORATOR = False
+        self.assertRaises(PreConditionFailed, self.run_example)
+
+    def test_1d_clean(self):
+        global BAD1, BAD2, BAD3, DECORATOR
+        BAD1 = BAD2 = BAD3 = False
+        DECORATOR = True
+        self.assertEqual(self.run_decorated(), 11)
+
+    def test_1d_bad1(self):
+        global BAD1, BAD2, BAD3, DECORATOR
+        BAD1 = True
+        BAD2 = BAD3 = False
+        DECORATOR = True
+        self.assertRaises(PostConditionFailed, self.run_decorated)
+
+    def test_1d_bad2(self):
+        global BAD1, BAD2, BAD3, DECORATOR
+        BAD2 = True
+        BAD1 = BAD3 = False
+        DECORATOR = True
+        self.assertRaises(InvariantChanged, self.run_decorated)
+
+    def test_1d_bad3(self):
+        global BAD1, BAD2, BAD3, DECORATOR
+        BAD3 = True
+        BAD1 = BAD2 = False
+        DECORATOR = True
+        self.assertRaises(PreConditionFailed, self.run_decorated)
+
+    def test_2_good(self):
+        self.assertEqual(example_2(1), 5)
+
+    def test_2_bad(self):
+        self.assertRaises(PostConditionFailed, lambda: example_2(25))
+
+    def test_3_good(self):
+        self.assertEqual(example_3(1), 2)
+
+    def test_3_bad1(self):
+        self.assertRaises(PreConditionFailed, lambda: example_3(1, 12, 100))
+
+    def test_3_bad2(self):
+        self.assertRaises(PreConditionFailed,
+                          lambda: example_3(1, 1, 2,
+                                            a='hi', b='bye', c='ouch'))
+
+if __name__ == "__main__":
+    unittest.main()
